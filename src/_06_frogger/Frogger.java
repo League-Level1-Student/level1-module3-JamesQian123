5c7aa7d5b1751ac5b5b7fbe5708315f1cc462c9f
league <league@adminisorsimac3.attlocal.net>
2023-06-17 11:24:32 -0700

    asdfas

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -1,15 +1,30 @@
 package _06_frogger;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 800;
-    int x;
-    int y;
+    int frogX = 400;
+    int frogY=600;
     Car tesla;
     Car honda;
     Car bmw;
+    PImage back;
+    PImage carLeft;
+    PImage carRight;
+    PImage frog;
+      
+      
+       
+//       public void draw() {
+//           background(back);
+//           image (carLeft,250,360);
+//           image (carRight,250, 210);
+//           image (frog, 300, 530);
+//       }
+        
     
     @Override
     public void settings() {
@@ -18,24 +33,57 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-    	tesla =  new Car(300,100,150,200);
-    	honda = new Car(400,500,100,150);
-    	bmw = new Car(490,294,281,128);
+    	tesla =  new Car(300,100,100,5);
+    	honda = new Car(400,500,100,5);
+    	bmw = new Car(490,242,100,5);
+    	back = loadImage("/Users/league/git/level1-module3-JamesQian123/src/_06_frogger/froggerBackground.png");
+    	back.resize(800, 600);
+    	
+    	
+        carLeft = loadImage("/Users/league/git/level1-module3-JamesQian123/src/_06_frogger/carLeft.png");
+        carLeft.resize(160,100);
+        carRight = loadImage("/Users/league/git/level1-module3-JamesQian123/src/_06_frogger/carRight.png");
+        carRight.resize(160,100);
+        frog = loadImage("/Users/league/git/level1-module3-JamesQian123/src/_06_frogger/frog.png");
+        frog.resize(75,75);
     }
 
     @Override
     public void draw() {
-    	background(103,197,245);
+    	 background(back);
     	fill(64,184,53);
-    	ellipse(x,y,50,50);
-    	if(x == 800 && y == 600) {
-    		x = 800;
-    		y = 600;
-    	}
+    	ellipse(frogX,frogY,50,50);
+    	
+    	//if(frogX == 800 && frogY == 600) {
+    		//frogX = 800;
+    		//frogY = 600;
+    	//}
     	tesla.display();
     	honda.display();
     	bmw.display();
+    	tesla.moveLeft();
+    	tesla.display();
+    	honda.moveRight();
+    	honda.display();
+    	bmw.moveLeft();
+    	bmw.display();
+    	if(intersects(tesla)||intersects(honda)||intersects(bmw)) {
+    		frogX = 400;
+    		frogY = 600;
+    		
+    	}
+    	
     }
+    boolean intersects(Car car) {
+  		 if ((frogY > car.getY() && frogY < car.getY()+50) &&
+  		                (frogX > car.getX() && frogX < car.getX()+car.getSize())) {
+  		   return true;
+  		  }
+  		 else  {
+  		  return false;
+  		 }
+  }
+    	
     
     public void keyPressed()
     {
@@ -43,31 +91,31 @@ public class Frogger extends PApplet {
             if(keyCode == UP)
             	
             {
-            	y+=1;
+            	frogY-=10;
                 //Frog Y position goes up
             }
             else if(keyCode == DOWN)
             {
-            	y-=1;
+            	frogY+=10;
                 //Frog Y position goes down 
             }
             else if(keyCode == RIGHT)
             {
-            	x+=1;
+            	frogX+=10;
                 //Frog X position goes right
             }
             else if(keyCode == LEFT)
             {
-            	x-=1;
+            	frogX-=10;
                 //Frog X position goes left
             }
         }
     }
     public class Car {
-    	int x;
-    	int y;
-    	int size;
-    	int speed;
+    	private int x;
+    	private int y;
+    	private int size;
+    	private int speed;
     	public Car(int x, int y, int size, int speed) {
     		 this.x = x;
     		 this.y = y;
@@ -75,13 +123,37 @@ public class Frogger extends PApplet {
     		 this.speed = speed;
     	}
     	void display(){
-    	    fill(0,255,0);
-    	    rect(x , y,  size, 50);
+    		image (carLeft,x,y);
+           
     	  }
-    	
-
-    }
+    	void moveLeft() {
+    		x-=speed;
+    		if(x < -size) {
+    			x = WIDTH;
+    			
+    		}
     
+    	}
+    	void moveRight() {
+    		x+=speed;
+    		if(x > WIDTH) {
+    			x = -size;
+    			
+    		}
+    	}
+    	public int getX() {
+    		return x;
+    	}
+    	public int getY() {
+    		return y;
+    	}
+    	public int getSize() {
+    		return size;
+    	}
+    	
+    	
+    }
+   
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
     }
